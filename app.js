15ad52e6577b4ff5a9b1142513de125fe2dd845e
Brendon Gomes Wildner <brendon.gwildner@gmail.com>
2024-10-23 18:25:40 -0300

    Primeira versão do app com 3 rotas

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+// Rota 1: Página inicial
+app.get('/', (req, res) => {
+  res.send('<h1>Bem-vindo à Página Inicial!</h1>');
+});
+
+// Rota 2: Página de Contato
+app.get('/contato', (req, res) => {
+  res.send('<h1>Contato: email@email.com</h1>');
+});
+
+// Rota 3: Página Sobre
+app.get('/sobre', (req, res) => {
+  res.send('<h1>Sobre Nós: Somos uma empresa fictícia!</h1>');
+});
+
+// Servidor rodando na porta 3000
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
